135f9fb3dd5854230a89e65a118f47dbc54c66f4
Cameron Dickins <camerondickins@gmail.com>
2021-12-04 01:42:01 -0500

    Add method to generate user-friendly string representing maze layout

--- Maze.py
+++ Maze.py
@@ -3,3 +3,8 @@ class Maze:
 		# Import maze from file
 		with open('start-maze', 'rt') as file:
 			self._map = [[char for char in list(line) if char != '\n'] for line in file]
+
+
+	def __str__(self) -> str:
+		# Provides string representing the maze arrangement
+		return '\n'.join(map(''.join, self._map))
